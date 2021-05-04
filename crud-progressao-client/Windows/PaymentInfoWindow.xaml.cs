73673857614392e7781cdb4c9cd0a51d5206b858
Vinícius Lucas <viniciuslucas1564@gmail.com>
2021-05-04 14:54:56 -0300

    Payment info window added

---
+++ crud-progressao-client/Windows/PaymentInfoWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace crud_progressao.Windows {
+    public partial class PaymentInfoWindow : Window {
+        public PaymentInfoWindow() {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void DeleteButton(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void CancelButton(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
