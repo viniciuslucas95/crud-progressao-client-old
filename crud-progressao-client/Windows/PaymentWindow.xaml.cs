315febb2ca82e06be5fe31070677204a942c0fdc
Vinícius Lucas <viniciuslucas1564@gmail.com>
2021-05-04 14:54:38 -0300

    Payment window added

---
+++ crud-progressao-client/Windows/PaymentWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace crud_progressao.Windows {
+    public partial class PaymentWindow : Window {
+        public PaymentWindow() {
+            InitializeComponent();
+        }
+
+        private void EditButton(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void CloseButton(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ReportButton(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void NewPaymentButton(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
